90b15116dc3bc81d5b98afa7d0c939a576db8b89
eugenia <orristurmurminintur@gmail.com>
2017-01-14 12:19:03 +0200

    Update Category model

--- app/models/category.server.model.js
+++ app/models/category.server.model.js
@@ -5,8 +5,10 @@ module.exports = function (mongoose) {
   const schema = new mongoose.Schema({
     name: {
       type: String,
+      unique: true,
       required: true
-    }
+    },
+    description: String
   }, {timestamps: true});
 
   return mongoose.model(modelName, schema);
